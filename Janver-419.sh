2b13d8208c5c37ed02fdd4d5aa567e652127617b
kfen <36830580+kfen@users.noreply.github.com>
2020-10-06 18:14:55 +0800

    Update and rename project-419.sh to Janver-419.sh

--- Janver-419.sh
+++ Janver-419.sh
@@ -9,15 +9,15 @@
 
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.2.1/g' ./package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.1/g' ./package/base-files/files/bin/config_generate
 
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/HAILANG/g' ./package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/HAILANG/g' ./package/base-files/files/bin/config_generate
 
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
+#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
 
 
 # 修改内核版本（版本内核默认4.19，还有4.14跟4.9内核）
